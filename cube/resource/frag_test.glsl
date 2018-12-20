06b42d42d199b7199f3d48de467f669ac1c4e7c8
linux_sucks <junha1@postech.ac.kr>
2018-12-21 07:05:06 +0900

    qweqeqweqeqwe

--- cube/resource/frag_test.glsl
+++ cube/resource/frag_test.glsl
@@ -49,6 +49,139 @@ float mynorm(float x)
 {
 	return 0.5 + 0.5 * tanh(x);
 }
+
+
+
+
+
+
+
+
+
+
+
+
+float tanhnorm(float x)
+{
+	return 0.5 + 0.5 * tanh(x);
+}
+
+float sinnorm(float x)
+{
+	return 0.5 + 0.5 * sin(x);
+}
+
+dvec2 zoomto(double zoom, vec2 coord, dvec2 center)
+{
+	return coord / zoom + center;
+}
+
+vec4 junhacolor(float a, float b)
+{
+	vec2 uv;
+	uv.y = 0.5;
+
+	uv.x = a;
+	vec4 color1 = texture2D(pallete, uv) * (1 - a);
+
+	uv.x = (1 - a) * b;
+	vec4 color2 = texture2D(pallete, uv) * (1 - a);
+
+	uv.x = sin(a*3.1415);
+	vec4 color3 = texture2D(pallete, uv) * (0.5* sin(t) + 0.5);
+
+
+	vec4 clr = color1 + color2 + color3;
+	clr = clr * 0.3333;
+	clr[3] = 1;
+
+	return clr;
+}
+
+vec4 haebincolor(float a, float b)
+{
+	vec2 uv;
+	uv.x = sinnorm(exp(exp(a)) * b);
+	uv.y = sinnorm(exp(exp(b)) * a);
+	vec4 color1 = texture2D(pallete, uv) * (1 - a) * a * b;
+
+	uv.x = sinnorm(exp(exp(a)));
+	uv.y = sinnorm(exp(exp(a)));
+	vec4 color2 = texture2D(pallete, uv) * (1 - a) * a;
+
+
+	vec4 clr = color1 * 2  +  color2 * 2.5;
+	clr[3] = 1;
+
+	return clr;
+
+}
+
+vec4 defaultmandelbrot(float a)
+{
+	vec4 clr = vec4(a, a, a, 1);
+	return clr;
+}
+
+vec4 noanti(vec2 point)
+{
+	double zoom = exp(sinnorm(t) * 20);
+	dvec2 cen = dvec2(-0.7280101473, 0.1945);
+	dvec2 c = zoomto(zoom, point, cen);
+
+	double x = c.x; // * sin(1/float(zoom + 1) * 0.9);
+	double y = c.y; // * cos(1/float(zoom + 1));
+
+	int max_iter = 200;
+
+	float max = 4;
+	
+
+	int i;
+	for(i = 0; i < max_iter; i++)
+	{
+		double nx = x*x-y*y + c.x;
+		double ny = 2*x*y + c.y;
+
+		if (nx*nx + ny * ny > max)
+			break;
+
+		x = nx;
+		y = ny;
+	}
+
+	float a = float(i) / (max_iter);
+	float b = tanhnorm(float(x/(y)));
+
+	
+	return haebincolor(a,b);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 void main()
 {
 
@@ -75,11 +208,11 @@ void main()
 	mcolor[2] = 0.5 + 0.5*sin(10/fractal1*(opos[0] + opos[2]));
 
 	mcolor = vec4(float(z[0] + 25)/(max_iter + 25), 0, 0, 1);
-
+	mcolor = vec4(1, 0, 0, 1);
 	mcolor[3] = 1;
 
 	vec4 amb_r = ambient;
-	color = vec4(0);
+	color = noanti(vec2(cr, ci));
 
 	for(int i = 0; i < 3; i++)
 	{
